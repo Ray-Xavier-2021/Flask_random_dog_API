c768815bc10e658038258c733001cd3b12881986
Ray-Xavier-2021 <rayxavier.frontenddev.2021@gmail.com>
2022-11-01 15:53:46 -0400

    created get dog and logout route

--- random_dog/main.py
+++ random_dog/main.py
@@ -11,6 +11,20 @@ app.config['SESSION_COOKIE_SECURE'] = True
 def home():
   return render_template('index.html')
 
+'''
+Create a get_dog route
+'''
+@app.route('/get_dog')
+def get_dog():
+  return 'Ruff..'
+
+'''
+Create route for logout
+'''
+@app.route('/logout')
+def logout():
+  return 'LOGOUT PAGE'
+
 if __name__ == "__main__":
     # Replit server config
     # app.run(debug=True,host='0.0.0.0', port=81)
